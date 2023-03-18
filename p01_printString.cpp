45ade0e43206c8d50c0d7ad1e223edbbcdbae2d5
Anitta Siby <anittasiby002@gmail.com>
2023-03-18 19:32:58 +0530

    Create p01_printString.cpp

---
+++ p01_printString.cpp
@@ -0,0 +1,16 @@
+//program to print a string on the screen 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+    return 0;
+}
+
+
+/*
+Output:-
+Hello World
+*/
