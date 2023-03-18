bf9a26c486b461910d1ce438ad0deea7d3293ff6
Anitta Siby <anittasiby002@gmail.com>
2023-03-18 22:08:54 +0530

    Create p08_areaOfSquare.cpp

---
+++ p08_areaOfSquare.cpp
@@ -0,0 +1,24 @@
+//program to compute area of a square
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float side;
+    
+    cout<<"enter the side of the square : ";
+    cin>>side;
+    
+    cout<<"The area of square is "<<side*side<<"\n";
+    
+    return 0;
+}
+
+
+/*
+Output:-
+
+enter the side of the square : 4
+The area of square is 16
+*/
