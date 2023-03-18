ab6e5f15289b90fc5b4f820bbc672d7441e17943
Anitta Siby <anittasiby002@gmail.com>
2023-03-18 19:39:23 +0530

    Create p02_printSum.cpp

---
+++ p02_printSum.cpp
@@ -0,0 +1,29 @@
+//program to print sum of two values
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2,sum;
+    
+    cout<<"enter num1 : ";
+    cin>>num1;
+    cout<<"enter num2 : ";
+    cin>>num2;
+    
+    sum = num1 + num2;
+    cout<<"The sum is : ";
+    cout<<sum;
+    
+    return 0;
+}
+
+
+/*
+Output:-
+
+enter num1 : 3
+enter num2 : 4
+The sum is : 7
+*/
